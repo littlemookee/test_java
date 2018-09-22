6ab6fe300ec205638af209c894b899b0d1e5b596
mikhail <zulkarneev@mail.ru>
2018-09-22 17:31:26 +0300

    Second variant

--- test_java/src/Arc.java
+++ test_java/src/Arc.java
@@ -1,13 +1,11 @@
 public class Arc
-{
-	private static final String NEWLINE = System.getProperty("line.separator");
+{	
+	final Integer iSymbol;
+	final Integer oSymbol;
+	final Double weight;
+	final Integer nextNode;
 	
-	final int iSymbol;
-	final int oSymbol;
-	final double weight;
-	final int nextNode;
-	
-	public Arc(int iSymbol, int oSymbol, double weight, int nextNode)
+	public Arc(Integer iSymbol, Integer oSymbol, Double weight, Integer nextNode)
 	{
 		this.iSymbol = iSymbol;
 		this.oSymbol = oSymbol;
@@ -17,20 +15,12 @@ public class Arc
 	
     /**
      * Returns a string representation of the Arc.
-     *
-     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,  
-     *         followed by the <em>V</em> adjacency lists
      */
-/*    public String toString() {
+    public String toString() {
         StringBuilder s = new StringBuilder();
-        s.append(V + " vertices, " + E + " edges " + NEWLINE);
-        for (int v = 0; v < V; v++) {
-            s.append(String.format("%d: ", v));
-            for (int w : adj[v]) {
-                s.append(String.format("%d ", w));
-            }
-            s.append(NEWLINE);
-        }
+        s.append(nextNode + " " + iSymbol + " " + oSymbol);
+        if (weight != 0.0) 
+            s.append(" " + weight);
         return s.toString();
-    }*/	
+    }	
 }
--- test_java/src/WFST.java
+++ test_java/src/WFST.java
@@ -1,68 +1,31 @@
 import edu.princeton.cs.algs4.*;
-import java.util.NoSuchElementException;
+//import java.util.NoSuchElementException;
 
 public class WFST
 {
-	private Integer numNodes;
-	private Integer numArcs;
-	private Integer numFinals;
-	private Bag<Arc>[] arcs;
-	private Integer[] finalNodes;
-	private Double[] finalWeights;
+	private static final String NEWLINE = System.getProperty("line.separator");
 	
-	private <T> void resize(T[] a, int newSize)
-	{
-		if (newSize == 0) newSize = 1;
-		T[] newA = (T[]) new Object[newSize];
-		for (int i=0; i<a.length; i++) newA[i] = a[i];
-		a = newA;
-	}
-	
-	private void addFinalNode(int node, double weight)
-	{
-		if (finalNodes.length <= numFinals) {
-			resize(finalNodes, 2*numFinals);
-			resize(finalWeights, 2*numFinals);
-		}
-		finalNodes[numFinals] = node;
-		finalWeights[numFinals++] = weight;
-	}
-	
-	private void addArc(int startNode, Arc arc)
-	{
-		if (arcs.length <= numNodes) {
-			resize(arcs, 2*numNodes);
-			arcs[numNodes] = new Bag<Arc>();
-		}
-		arcs[startNode].add(arc);
-		numArcs++;
-	}
-	
+	private final ST<Integer,Bag<Arc>> arcs;
+	private final Bag<FinalNode> finalNodes;
+		
 	public WFST(String fileName)
 	{
+		arcs = new ST<Integer,Bag<Arc>>();
+		finalNodes = new Bag<FinalNode>();
 		In in = new In(fileName);
-		ST<Integer,Integer> st = new ST<Integer,Integer>();
-		
-		numNodes = 0;
-		numArcs = 0;
-		numFinals = 0;
-		
 		while (!in.isEmpty()) {
 			String line = in.readLine();
 			String[] splited = line.split("\\s+");
 			switch (splited.length) {
 				case 1: {
-					int node = Integer.parseInt(splited[0]);
-					double weight = 0.0;
-					if (!st.contains(node)) st.put(node, numNodes++);
-					addFinalNode(st.get(node), weight);
+					Integer node = Integer.parseInt(splited[0]);					
+					finalNodes.add(new FinalNode(node, 0.0));
 				}
 				break;
 				case 2: {
-					int node = Integer.parseInt(splited[0]);
-					double weight = Double.parseDouble(splited[1]);
-					if (!st.contains(node)) st.put(node, numNodes++);
-					addFinalNode(st.get(node), weight);
+					Integer node = Integer.parseInt(splited[0]);
+					Double weight = Double.parseDouble(splited[1]);
+					finalNodes.add(new FinalNode(node, weight));					
 				}
 				break;
 				case 4: {
@@ -70,40 +33,49 @@ public class WFST
 					int endNode = Integer.parseInt(splited[1]);
 					int iSymbol = Integer.parseInt(splited[2]);
 					int oSymbol = Integer.parseInt(splited[3]);
-					double weight = 0.0;
 					
-					if (!st.contains(startNode))
-						st.put(startNode, numNodes++);
-					if (!st.contains(endNode))
-						st.put(endNode, numNodes++);
-					
-					addArc(st.get(startNode), new Arc(iSymbol, oSymbol, weight, st.get(endNode)));					
+					Bag<Arc> outArcs;
+					if (arcs.contains(startNode)) outArcs = arcs.get(startNode);
+					else 						  outArcs = new Bag<Arc>();
+					outArcs.add(new Arc(iSymbol,oSymbol,0.0,endNode));
+					arcs.put(startNode, outArcs);
 				}
 				break;
 				case 5: {
 					int startNode = Integer.parseInt(splited[0]);
 					int endNode = Integer.parseInt(splited[1]);
 					int iSymbol = Integer.parseInt(splited[2]);
-					int oSymbol = Integer.parseInt(splited[3]);
-					double weight = Integer.parseInt(splited[4]);
+					int oSymbol = Integer.parseInt(splited[3]);					
+					double weight = Double.parseDouble(splited[4]);
 					
-					if (!st.contains(startNode))
-						st.put(startNode, numNodes++);
-					if (!st.contains(endNode))
-						st.put(endNode, numNodes++);
-					
-					addArc(st.get(startNode), new Arc(iSymbol, oSymbol, weight, st.get(endNode)));					
+					Bag<Arc> outArcs;
+					if (arcs.contains(startNode)) outArcs = arcs.get(startNode);
+					else 						  outArcs = new Bag<Arc>();
+					outArcs.add(new Arc(iSymbol,oSymbol,weight,endNode));
+					arcs.put(startNode, outArcs);
 				}
 				break;
 				default:
 					throw new IllegalArgumentException("Wrong format of WFST line: " + line);
 			}
         }
-	}	
-	
-	public static void main(String[] args)
-	{
-		
-		
 	}
+	
+    /**
+     * Returns a string representation of the Arc.
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Integer key : arcs.keys())
+        	for (Arc arc : arcs.get(key))
+        		s.append(key + " " + arc + NEWLINE);        
+        for (FinalNode node : finalNodes)
+        	s.append(node + NEWLINE);
+        return s.toString();
+    }	
+	
+    public static void main(String[] args) {    	
+        WFST wfst = new WFST(args[0]);
+        StdOut.println(wfst);
+    }
 }
