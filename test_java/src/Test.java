2d80e2dfb4289095f04aa02e9dd11305da225090
mikhail <zulkarneev@mail.ru>
2018-10-06 16:48:34 +0300

    Test class

--- test_java/src/Test.java
+++ test_java/src/Test.java
@@ -1,15 +1,19 @@
+import edu.princeton.cs.algs4.*;
 
 public class Test {
-	public static void main(String[] args) {		
-		int N = 10;
-		int[] a = new int[N];
-		int k = 3;
+	private static final String NEWLINE = System.getProperty("line.separator");
+	
+	private final int[] a;
+
+	public Test(int N, int k) {
+		a = new int[N];
 		
 		for (int i=0; i<N; i++) a[i] = i;
 
 		int processedCount = 0; 
 		int baseIndex = 0;
-		while (processedCount<N)
+
+		while (processedCount < N)
 		{
 			int curElement = a[baseIndex];
 			int nextIndex = (baseIndex + k) % N;			
@@ -22,7 +26,25 @@ public class Test {
 			}
 			baseIndex = nextIndex + 1;
 		}
-		
-		int l = 0;		
+	}
+	
+	public int[] getArray() {
+		return a;
+	}
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int i=0; i<a.length; i++) {
+        	if (i > 0)
+        		s.append(" ");        
+       		s.append(a[i]);
+        }
+       	s.append(NEWLINE);
+        return s.toString();
+    }
+	
+	public static void main(String[] args) {
+		Test test = new Test(10,4);
+		StdOut.println(test.toString());
 	}
 }
